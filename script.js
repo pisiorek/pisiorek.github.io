e8b304c88cb0ae25a528f4f165017fef83f9b49c
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-05-17 15:30:29 +0200

    tt

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   const apiRoot = 'https://desolate-brook-46706.herokuapp.com/v1/task/';
   //const apiRoot = 'http://localhost:8080/v1/task/';
-  //const trelloApiRoot = 'http://localhost:8080/v1/trello/';
-  const trelloApiRoot = 'https://desolate-brook-46706.herokuapp.com/v1/trello/';
+  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  //const trelloApiRoot = 'https://desolate-brook-46706.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
