020b49bc28a426b979a47399e10f22703bbfe4e8
Dariusz Harańczyk <darek.haranczyk@gmail.com>
2019-06-04 08:48:48 +0200

    zmiana apiRoot na Heroku

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  //const apiRoot = 'https://desolate-brook-46706.herokuapp.com/v1/task/';
-  const apiRoot = 'http://localhost:8080/v1/';
+  const apiRoot = 'https://fathomless-forest-89199.herokuapp.com/v1/';
+  //const apiRoot = 'http://localhost:8080/v1/';
   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   //const trelloApiRoot = 'https://desolate-brook-46706.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
